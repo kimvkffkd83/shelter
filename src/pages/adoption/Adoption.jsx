a5230c806194b041ed725bee37a7208f8fd1ed63
kimvkffkd83 <se6651@gmail.com>
2024-07-04 23:50:56 +0900

    STYLE: Modify style of adoption info page

--- src/pages/adoption/Adoption.jsx
+++ src/pages/adoption/Adoption.jsx
@@ -68,25 +68,37 @@ function Adoption() {
                 <>
                     <>
                         <h3>양식 다운로드</h3>
-                        <ul>
-                            <li>
-                                <label className="file__title" htmlFor="download1">입양 설문지(개) : </label>
-                                <button className="btn__download" id="download1" onClick={()=>downloadFile('adoptFormDog')}>다운로드</button>
-                            </li>
-                            <li>
-                                <label className="file__title" htmlFor="download2">입양 설문지(고양이) : </label>
-                                <button className="btn__download" id="download2" onClick={()=>downloadFile('adoptFormCat')}>다운로드</button>
-                            </li>
-                        </ul>
+                        <div className="download__box">
+                            <div className="download__item">
+                                <label className="file__title" htmlFor="download1"><strong>입양 설문지(개) : </strong></label>
+                                <button className="file__btn" id="download1"
+                                        onClick={() => downloadFile('adoptFormDog')}>
+                                    <span className="material-symbols-outlined">download</span>
+                                    내려받기
+                                </button>
+                            </div>
+                            <div className="download__item">
+                                <label className="file__title" htmlFor="download2"><strong>입양 설문지(고양이)
+                                    : </strong></label>
+                                <button className="file__btn" id="download2"
+                                        onClick={() => downloadFile('adoptFormCat')}>
+                                    <span className="material-symbols-outlined">download</span>
+                                    내려받기
+                                </button>
+                            </div>
+                        </div>
                     </>
-                    {
-                        isAdmin &&
-                        <div>
+
+                    <div>
+                        <h3>과정 및 절차</h3>
+                        {
+                            isAdmin &&
                             <button className="btn__adm__icon btn__adm__write" onClick={write}>
                                 <span className="material-symbols-outlined">bookmark_manager</span>
                             </button>
-                        </div>
-                    }
+                        }
+                    </div>
+
                     <div className="tab__header">
                         {
                             list.map((tab, idx) => (
